3eb26b38300dfe77628fb1ccb78515d80baa0bff
suncedd <73060347+suncedd@users.noreply.github.com>
2022-06-15 17:16:45 +0500

    Add files via upload

---
+++ Курсач/Курсач/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Курсач
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection sqlcon = new SqlConnection(@"Data Source=PK306NEW-7\SQLEXPRESS;Initial Catalog=sport;Integrated Security=True");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string stat;
+        public static int kod;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            sqlcon.Open();
+            string log = textBox1.Text;
+            string par = textBox2.Text;
+            string l = "";
+
+
+            SqlCommand commands = new SqlCommand("USE [sport] SELECT * FROM [DBO].Авторизация WHERE [DBO].Авторизация.[Логин] = '"+log+"' ", sqlcon);
+            SqlDataReader read = commands.ExecuteReader();
+            while (read.Read())
+            {
+                l = read.GetValue(1).ToString();
+            }
+                if (l == log)
+                {
+                    string p = "";
+                  
+                sqlcon.Close();
+                sqlcon.Open();
+                    commands = new SqlCommand("USE [sport] SELECT * FROM [DBO].Авторизация WHERE [DBO].Авторизация.[Пароль] = '"+par+"' and  [DBO].Авторизация.[Логин] = Логин", sqlcon);
+                SqlDataReader read3 = commands.ExecuteReader();
+                while (read3.Read())
+                {
+                    p = read3.GetValue(2).ToString();
+                    kod = (int)read3.GetValue(3);
+                }
+                    if (p == par)
+                    {
+                        sqlcon.Close();
+
+                        sqlcon.Open();
+                        int dol = 0;
+                        SqlCommand commandss = new SqlCommand("USE [sport] SELECT * FROM [DBO].Сотрудники WHERE [DBO].Сотрудники.[КодСотрудника]= "+kod+"  ", sqlcon);
+                        SqlDataReader read2 = commandss.ExecuteReader();
+                    while (read2.Read())
+                    {
+                        dol = (int)read2.GetValue(2);
+                        stat = read2.GetValue(3).ToString();
+                    }
+                       if(stat == "Уволен")
+                    {
+                        MessageBox.Show("Вы уволены");
+                    }
+                    else
+                    {
+                        if (dol == 1)
+                        {
+
+                            ActiveForm.Hide();
+                            Админ adm = new Админ();
+                            adm.Show();
+                        }
+                        else if (dol == 2)
+                        {
+                            ActiveForm.Hide();
+                            менеджер men = new менеджер();
+                            men.Show();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Проверьте правильность введенных данных");
+                        }
+                        sqlcon.Close();
+                    }
+                        
+                      
+                    }
+                }
+           
+            sqlcon.Close();
+
+
+
+        }
+    }
+}
---
+++ Курсач/Курсач/РегистрацияКлиента.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Курсач
+{
+    public partial class РегистрацияКлиента : Form
+    {
+        public int kod;
+        public РегистрацияКлиента()
+        {
+            InitializeComponent();
+        }
+        SqlConnection sqlcon = new SqlConnection(@"Data Source=PK306NEW-7\SQLEXPRESS;Initial Catalog=sport;Integrated Security=True");
+        private void РегистрацияКлиента_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "sportDataSet4.Клиент". При необходимости она может быть перемещена или удалена.
+            this.клиентTableAdapter.Fill(this.sportDataSet4.Клиент);
+            sqlcon.Open();
+            DataSet ds = new DataSet();
+
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT[КодКлиента], Статус.Статус,[ФИО],[ДатаРождения], Абонемент.Название FROM([sport].[dbo].[Клиент] left join Статус on Клиент.КодСтатуса = Статус.КодСтатуса) left join Абонемент on Клиент.КодАбонемента = Абонемент.КодАбонемента", sqlcon);
+            adapter.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+            sqlcon.Close();
+            dataGridView1.Columns[0].Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string fio = textBox1.Text;
+            int passport = int.Parse(textBox2.Text);
+            string dat = dateTimePicker1.Text; 
+            
+            sqlcon.Open();
+            DataSet dss = new DataSet();
+            SqlDataAdapter command = new SqlDataAdapter("INSERT INTO[dbo].[Клиент] ([КодСотрудника],[КодСтатуса],[ФИО],[ДатаРождения],[Паспорт]) VALUES("+Form1.kod +",1,'"+fio+"','"+dat+"',"+passport+")", sqlcon);
+            command.Fill(dss);
+            sqlcon.Close();
+
+            sqlcon.Open();
+            DataSet ds = new DataSet();
+
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT[КодКлиента], Статус.Статус,[ФИО],[ДатаРождения], Абонемент.Название FROM([sport].[dbo].[Клиент] left join Статус on Клиент.КодСтатуса = Статус.КодСтатуса) left join Абонемент on Клиент.КодАбонемента = Абонемент.КодАбонемента", sqlcon);
+            adapter.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+            sqlcon.Close();
+            dataGridView1.Columns[0].Visible = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+
+
+
+            if (comboBox2.SelectedItem.ToString() == "")
+            {
+                MessageBox.Show("Выберите Статус");
+                return;
+            }
+
+
+
+
+            int ind =Convert.ToInt32(comboBox2.SelectedIndex+1);
+
+
+            sqlcon.Open();
+            DataSet dss = new DataSet();
+            SqlDataAdapter command = new SqlDataAdapter("Use [sport] update dbo.[Клиент] set  [КодСтатуса] = " + ind + " where dbo.Клиент.КодКлиента = " + Convert.ToInt32(comboBox1.SelectedValue) + "", sqlcon);
+            command.Fill(dss);
+            sqlcon.Close();
+
+            sqlcon.Open();
+            DataSet ds = new DataSet();
+
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT[КодКлиента], Статус.Статус,[ФИО],[ДатаРождения], Абонемент.Название FROM([sport].[dbo].[Клиент] left join Статус on Клиент.КодСтатуса = Статус.КодСтатуса) left join Абонемент on Клиент.КодАбонемента = Абонемент.КодАбонемента", sqlcon);
+            adapter.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+            sqlcon.Close();
+            dataGridView1.Columns[0].Visible = false;
+
+        }
+    }
+}
